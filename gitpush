2a3d6c311b14c281b6be6b432ee6f9b4932b7e52
jroliver81 <jasmineoliver81@yahoo.com>
2023-07-05 22:45:42 +0000

    minipulation lists

---
+++ gitpush
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+ 
+
+#ask for commit message
+echo "Enter the commit msg: "
+
+ 
+
+#save input to var message
+read message
+
+ 
+
+#git commands executed
+git add *
+git commit -m "$message"
+git push origin HEAD
---
+++ prelist.py
@@ -0,0 +1,6 @@
+wordbank= ["indentation", "spaces"] 
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+wordbank.append(4)
+num= input("Pick a student number!")
+num= int(input("Pick a student number!"))
+
