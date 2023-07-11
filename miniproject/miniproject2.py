a92d1be78d3113bc3282866baaceef20682378f0
jroliver81 <jasmineoliver81@yahoo.com>
2023-07-11 20:56:48 +0000

    Working with NADA APIs

--- miniproject/miniproject2.py
+++ miniproject/miniproject2.py
@@ -11,6 +11,7 @@ questions = [
 def menu():
     print("let's play a game!")
     print("I will give you a quote from a movie and you name that movie!")
+    print("Type 'done' to quit at any time.")
     print("Good Luck!")
 
 print("What should I call you?")
@@ -19,21 +20,25 @@ print("Heyy,", you + "!")
 
 round = 0
 answer = " "
+score = 0
 
 for question in questions:
 	print( " The game starts now!:" +question)
-score = 0
 
 while user_answer.lower() != question["answer"].lower() :
     print(f"Boooo tomatoes tomatoes! The movie is {question['answer']}.")
 user_answer = input()
 
+if user_answer.lower() == 'done':
+    break
 
 if user_answer.lower() == question["answer"].lower():
     print("Whew! You had me worried for a second!")
 score += 1
 
-else
+elif round == 2:
+#figure out if I need to use elif or else with the print below or if the print below will be under while if i need it to try again.. but i need to get rounds to work in order to get the elif or else .
+else:
     print("You won a knuckle sandwich! Try again lol")
 
 
