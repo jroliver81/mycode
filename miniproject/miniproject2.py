5fbfedb933879e7b0f963b9c21729f43fee03349
jroliver81 <jasmineoliver81@yahoo.com>
2023-07-11 15:43:06 +0000

    Playing with python pip packages

--- miniproject/miniproject2.py
+++ miniproject/miniproject2.py
@@ -1,27 +1,41 @@
+#question = list
+#”question” & “answer” = dictionary
+# this is three dictionaries
+
 questions = [
         {"question": 'Name the movie this quote is from, "All my life I had to fight!"', "answer": "The Color Purple"},
         {"question": 'Name the movie this quote is from, " Im tired of this, Grandpa...Well thats too d*mn bad!"', "answer": 'Holes'},
         {"question": 'Name the movie this quote is from, " You been tripping every since I met you."', "answer": "Money Talks"}
-              ]  
+              ]
 
 def menu():
-    print("let's play a game!") 
+    print("let's play a game!")
     print("I will give you a quote from a movie and you name that movie!")
     print("Good Luck!")
 
 print("What should I call you?")
 you = input()
+print("Heyy,", you + "!")
 
 round = 0
 answer = " "
 
-for item in questions:
-    while round < 3 and answer != item['answer']:
-    round += 1     
-    answer = input(item['question'])
-    if answer == item['answer']:
-        print("Whew! You had me worried for a second!")
-    elif round == 3:    
-        print(f"Sorry, the movie was {item['answer']}, you should check it out!")
-    else:               
-        print("You won a knuckle sandwich! Try again lol")
+for question in questions:
+	print( " The game starts now!:" +question)
+score = 0
+
+while user_answer.lower() != question["answer"].lower() :
+    print(f"Boooo tomatoes tomatoes! The movie is {question['answer']}.")
+user_answer = input()
+
+
+if user_answer.lower() == question["answer"].lower():
+    print("Whew! You had me worried for a second!")
+score += 1
+
+else
+    print("You won a knuckle sandwich! Try again lol")
+
+
+print(f"\nThats a wrap folks! Your score is {score}/{len(questions)}.")
+
