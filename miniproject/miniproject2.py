0eb9969c97636360d5cff31f742c13175911f779
jroliver81 <jasmineoliver81@yahoo.com>
2023-07-11 03:04:44 +0000

    miniproject

--- miniproject/miniproject2.py
+++ miniproject/miniproject2.py
@@ -1,5 +1,7 @@
 questions = [
-        {"question": 'Name the movie this quote is from, "All my life I had to fight!"', "answer": "The Color Purple", "question": 'Name the movie this quote is from, " Im tired of this, Grandpa...Well thats too d*mn bad!"', "answer": 'Holes', "question": 'Name the movie this quote is from, " You been tripping every since I met you."', "answer": "Money Talks"}
+        {"question": 'Name the movie this quote is from, "All my life I had to fight!"', "answer": "The Color Purple"},
+        {"question": 'Name the movie this quote is from, " Im tired of this, Grandpa...Well thats too d*mn bad!"', "answer": 'Holes'},
+        {"question": 'Name the movie this quote is from, " You been tripping every since I met you."', "answer": "Money Talks"}
               ]  
 
 def menu():
@@ -13,12 +15,13 @@ you = input()
 round = 0
 answer = " "
 
-while round < 3 and answer != "The Color Purple":
+for item in questions:
+    while round < 3 and answer != item['answer']:
     round += 1     
-    answer = input('Name the movie this quote is form, "All my life I had to fight!": ')
-    if answer == "The Color Purple":
+    answer = input(item['question'])
+    if answer == item['answer']:
         print("Whew! You had me worried for a second!")
     elif round == 3:    
-        print("Sorry, the movie was The Color Purple, you should check it out!")
+        print(f"Sorry, the movie was {item['answer']}, you should check it out!")
     else:               
         print("You won a knuckle sandwich! Try again lol")
