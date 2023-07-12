3d3053fd77a9045fe3c904a9d6045a716e8b7505
jroliver81 <jasmineoliver81@yahoo.com>
2023-07-12 16:53:04 +0000

    A bit on error handling

--- miniproject/miniproject2.py
+++ miniproject/miniproject2.py
@@ -1,46 +1,52 @@
 #question = list
-#”question” & “answer” = dictionary
+#"question" & “answer” = dictionary
 # this is three dictionaries
 
+import random
 questions = [
         {"question": 'Name the movie this quote is from, "All my life I had to fight!"', "answer": "The Color Purple"},
         {"question": 'Name the movie this quote is from, " Im tired of this, Grandpa...Well thats too d*mn bad!"', "answer": 'Holes'},
         {"question": 'Name the movie this quote is from, " You been tripping every since I met you."', "answer": "Money Talks"}
               ]
 
-def menu():
+def main():
     print("let's play a game!")
     print("I will give you a quote from a movie and you name that movie!")
     print("Type 'done' to quit at any time.")
     print("Good Luck!")
 
-print("What should I call you?")
-you = input()
-print("Heyy,", you + "!")
+    print("What should I call you?")
+    you = input()
+    print("Heyy,", you + "!")
 
-round = 0
-answer = " "
-score = 0
+    score = 0
+#    retry = false
+    for i in range(3):
+        question = random.choice(questions)
+        print(question["question"])
+        user_answer = input()
 
-for question in questions:
-	print( " The game starts now!:" +question)
+        if user_answer.lower() == 'done':
+            break
 
-while user_answer.lower() != question["answer"].lower() :
-    print(f"Boooo tomatoes tomatoes! The movie is {question['answer']}.")
-user_answer = input()
+        if user_answer.lower() == question["answer"].lower():
+            print("Whew! You had me worried for a second!")
+            score += 1
+ #           retry = true
+            questions.remove(question)
 
-if user_answer.lower() == 'done':
-    break
+        elif user_answer.lower() != question["answer"].lower():
+            print("You won a knuckle sandwich! Try again lol")
+  #          retry = false
+   #     if retry:
+    #        user_answer.lower() != question["answer"].lower():
+     #       pass
 
-if user_answer.lower() == question["answer"].lower():
-    print("Whew! You had me worried for a second!")
-score += 1
+        else:
+            print(f"Boooo tomatoes tomatoes! The movie is {question['answer']}.")
 
-elif round == 2:
-#figure out if I need to use elif or else with the print below or if the print below will be under while if i need it to try again.. but i need to get rounds to work in order to get the elif or else .
-else:
-    print("You won a knuckle sandwich! Try again lol")
+    if score > 0:
+        print(f"\nThats a wrap folks! Your score is {score}/{len(questions)}.")
 
-
-print(f"\nThats a wrap folks! Your score is {score}/{len(questions)}.")
-
+if __name__ == "__main__":
+    main()
